9e6ca101066dee60f4dbaf5e73c7e3863d4cbb51
Wehrum <connor12568@gmail.com>
2021-11-02 18:22:00 -0700

    Even more physics updates
    - Added camera rotation, gravity scaler

--- Assets/Scripts/Mechanics/FlipGravity.cs
+++ Assets/Scripts/Mechanics/FlipGravity.cs
@@ -6,21 +6,30 @@ public class FlipGravity : MonoBehaviour
 {
     private PlayerMover player;
     private bool top;
-
     private Rigidbody2D rb;
+    private float GravDefault = 1f;
+    public float Gravity = 1f;
+    public bool CameraFlips = true;
+    private GameObject Camera;
 
     public bool Top { get => top; set => top = value; }
 
     void Start()
     {
         player = GetComponent<PlayerMover>();
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
+        Camera = GameObject.Find("CM vcam1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(GravDefault != Gravity)
+        {
+            GravDefault = Gravity;
+            rb.gravityScale = Gravity;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Is this working?");
             rb.gravityScale *= -1;
@@ -31,10 +40,12 @@ public class FlipGravity : MonoBehaviour
     {
         if (!Top)
         {
+            Camera.transform.eulerAngles = new Vector3(-180f, 0, 0);
             transform.eulerAngles = new Vector3(0, 0, 180f);
         }
         else
         {
+            Camera.transform.eulerAngles = Vector3.zero;
             transform.eulerAngles = Vector3.zero;
         }
         player.FacingRight = !player.FacingRight;
